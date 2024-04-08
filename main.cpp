45baece6584cb36d78a517d055a53399781f259c
EleniChristopoulou <132460411+EleniChristopoulou@users.noreply.github.com>
2024-04-08 17:15:12 +0300

    Display Score

--- main.cpp
+++ main.cpp
@@ -56,7 +56,6 @@ void drawMap(){
     
     glColor3ub(200, 0, 0);
 
-    int i=0, j=0;
     for(int i=0; i<map_height; i++){
         for(int j=0; j<map_width; j++){
         if(map[i][j] == 0){
@@ -70,6 +69,20 @@ void drawMap(){
         }
         }
     }
+}
+
+int getPoints(){
+    int points = 0;
+
+    for(int i=0; i<map_height; i++){
+        for(int j=0; j<map_width; j++){
+            if(map[i][j] == 1){
+                points++;
+            }
+        }
+    }
+
+    return points;
 }
 
 int checkColision(float nextPositionX, float nextPositionY){
@@ -130,7 +143,7 @@ void pacmanEat(){
 
     if(map[y][x] == 1){
         map[y][x] = 2;
-        score++;
+        // score++;
     }
 }
 
@@ -161,43 +174,48 @@ void timer(int) //1 fps
     glutTimerFunc(1000.0 / 60.0, timer, 0);
 }
 
-// void type(char *str, float x, float y, float s)
-// {
-//     glLineWidth(3);
-//     glEnable(GL_LINE_STIPPLE);
-//     glLineStipple(1, 0xFFFF);
-//     char *c;
-//     glPushMatrix();
-//     glTranslatef(y, x, 0);
-//     glScalef(0.1 * s, 0.1 * -s, 0);
-//     for (c = str; *c != '\0'; c++)
-//     glutStrokeCharacter(GLUT_STROKE_ROMAN, *c);
-//     glScalef(1, 1, 1);
-//     glPopMatrix();
-// }
+void type(char *str, float x, float y, float s)
+{
+    glLineWidth(3);
+    glEnable(GL_LINE_STIPPLE);
+    glLineStipple(1, 0xFFFF);
+    char *c;
+    glPushMatrix();
+    glTranslatef(y, x, 0);
+    glScalef(0.1 * s, 0.1 * -s, 0);
+    for (c = str; *c != '\0'; c++)
+    glutStrokeCharacter(GLUT_STROKE_ROMAN, *c);
+    glScalef(1, 1, 1);
+    glPopMatrix();
+}
 
 void display(){
     
     glClearColor(0.0f,0.0f,0.0f, 0.0f);   //background color
     glClear(GL_COLOR_BUFFER_BIT);
 
-    if(!gameOver){
+    if(getPoints()!=0){
         drawMap();
         drawPoints();
+
         drawPacMan(pacmanX,pacmanY);
         pacmanEat();    
-
         
-        // glColor3ub(255,255,255);;         //add score
-        // sprintf(message, "%d", score);
-        // type(message, 0, window_height/3, 3);
+        char str[] = "Points: ";            //Add score
+        glColor3ub(255,255,255);;         
+        sprintf(message, "%d", 220-getPoints());
+        strcat(str, message);
+        strcat(str, "/220");
+        type(str, -25, 10, 2);
 
         if(keySpecialStates[GLUT_KEY_UP] && checkColision(pacmanX, pacmanY-speed)!=0 ) {pacmanY -= speed; direction = 3*M_PI/2;}
         if(keySpecialStates[GLUT_KEY_DOWN] && checkColision(pacmanX, pacmanY+speed)!=0) {pacmanY += speed; direction = M_PI/2;}
         if(keySpecialStates[GLUT_KEY_LEFT] && checkColision(pacmanX-speed, pacmanY)!=0) {pacmanX -= speed; direction = M_PI;}
         if(keySpecialStates[GLUT_KEY_RIGHT] && checkColision(pacmanX+speed, pacmanY)!=0) {pacmanX += speed; direction = 0;}
     }else{
+        glColor3ub(255,255,255);
         
+        type("Game Over", window_height/2, window_width/2-70, 2);
     }
 
     glutSwapBuffers();
@@ -210,12 +228,12 @@ int main(int argc, char** argv)
 {
  glutInit(&argc,argv);
  glutInitWindowPosition(350, 0);
- glutInitWindowSize(window_width, window_height); // window_height -> window_height+50
+ glutInitWindowSize(window_width, window_height+70); // window_height -> window_height+50
  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
  glutCreateWindow("OpenGL Pacman");
  glMatrixMode(GL_PROJECTION);
  glLoadIdentity();
- gluOrtho2D(0,window_width,window_height,0);        // 0 -> -50
+ gluOrtho2D(0,window_width,window_height,-70);        // 0 -> -50
  glMatrixMode(GL_MODELVIEW);
 
  glutDisplayFunc(display);
